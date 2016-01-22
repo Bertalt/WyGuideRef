05d7974e80c6f9496c6fd8cdc2f6e47637177cee
bertalt <nowlvs@gmail.com>
2016-01-22 15:01:46 +0200

    First refactor

--- app/src/main/java/com/sls/wyguide/AutorizationActivity.java
+++ app/src/main/java/com/sls/wyguide/AutorizationActivity.java
@@ -1,14 +1,10 @@
 package com.sls.wyguide;
 
 import android.content.Intent;
-import android.graphics.Typeface;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 
 public class AutorizationActivity extends AppCompatActivity {
